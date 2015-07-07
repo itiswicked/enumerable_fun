a7ce7cc053a31e683c90186c8d59d0ec7cadef28
Nathan <nathanrbourke@gmail.com>
2015-07-07 19:51:18 -0400

    work through majority of method definitions

--- enumerable_fun.rb
+++ enumerable_fun.rb
@@ -1,62 +1,62 @@
-# [ [1, 2, 3, 4, 5],
-#   [4, 3, 5, 6, 2],
-#   [2, 1, 7, 6, 0] ]
-# 
-# Can you tell/show me:
-# 
-# - How many times each number appears
-# - Which number(s) appear(s) the most
-# - Which number(s) appear(s) the least
-# - The sum of all numbers
-# - How many numbers there are total
-# - Whether or not the total is prime
-# - The average value (sum of all numbers / count of numbers)
-# - All of the numbers sorted ascending
-# - All of the numbers sorted descending (just using a sort function)
-# - All of the numbers sorted descending (by reversing the array that's
-# sorted ascending)
-# - A list of unique numbers, sorted ascending (so [0, 1, 2, 3, 4, 5, 6,
-# 7])
-# - An array representing the sum of each column (so [7, 6, 15, ...]
-
 class MyEnumerable
 
   def count_each(items)
+    items.flatten.reduce({}) do |hash, item|
+      hash[item] ? hash[item] += 1 : hash[item] = 1
+      hash
+    end
   end
 
   def most_occuring(items)
+    hash = count_each(items)
+    items.flatten.reduce([]) do |memo, item|
+      if hash[item] == hash[memo.last] && item != memo.last
+        memo << item
+      elsif memo.empty? || hash[item] > hash[memo.last] 
+        memo = [item] 
+      end
+      memo
+    end
   end
 
   def least_occuring(items)
+    hash = count_each(items)
+    min_value = hash.values.min
+    hash.select { |key, value| value == min_value }.keys
   end
 
   def sum_of(items)
+    items.flatten.reduce(:+)
   end
 
   def size_of(items)
+    items.flatten.size
   end
 
   def sum_prime?(items)
+    n = sum_of(items)
+    (2..Math.sqrt(n)).all? { |number| n % number != 0 }
   end
 
   def average_of(items)
+    sum_of(items).to_f / size_of(items)
   end
 
-  def sort_asc(items)
+  def sort_asc(items);end
+
+  def sort_desc(items);end
+
+  def uniques(items)
+    count_each(items).reduce([]) { |uniqs, (key, value)| 
+      uniqs << key if value == 1; uniqs
+    }.sort
   end
 
-  def sort_desc(items)
-  end
-
-  def uniq(items)
-  end
-
-  def sum_columns(items)
-  end
+  def sum_columns(items);end
 
   private
 
-  def rotate_2d_ary(items)
-  end
+  # may be useful helper for sum_colums
+  def rotate_2d_ary(items);end
 
 end
--- enumerable_fun_spec.rb
+++ enumerable_fun_spec.rb
@@ -3,20 +3,19 @@ require_relative 'enumerable_fun'
 describe MyEnumerable do
 
   before(:each) do
-    @items = [ [1, 2, 3, 4, 5],
-              [4, 3, 5, 6, 2],
-              [2, 1, 7, 6, 0] ]
+    @items       = [ [1, 2, 3, 4, 5],
+                     [4, 3, 5, 6, 2],
+                     [2, 1, 7, 6, 0] ]
     @other_items = [5, 8, 6, 4, 3, 99, 4, 82, 1]
-    [5,8,6,3,99,82,1]
     @e = MyEnumerable.new
   end
 
   it 'counts each item in collection' do
-    expect(@e.count_each([])).to eq( [] )
+    expect(@e.count_each([])).to eq( {} )
     expect(@e.count_each([1,2,1,2])).to eq( { 1 => 2, 2 => 2 } )
-    expect(@e.count_each(@items)).to eq( { 1 => 2, 2 => 3, 3 => 2,
-                                         4 => 2, 5 => 2, 6 => 2,
-                                         7 => 1, 0 => 1 } )
+    expect(@e.count_each(@items)).to eq( 
+      { 1 => 2, 2 => 3, 3 => 2, 4 => 2, 
+        5 => 2, 6 => 2, 7 => 1, 0 => 1 } )
   end
 
   it 'returns which item(s) appear most' do
@@ -40,8 +39,8 @@ describe MyEnumerable do
   end
 
   it 'determines if sum of collection is prime' do
-    expect(@e.sum_prime?([53])).to eq(:true)
-    expect(@e.sum_prime?(@items)).to eq(:false)
+    expect(@e.sum_prime?([53])).to eq(true)
+    expect(@e.sum_prime?(@items)).to eq(false)
   end
 
   it 'returns average value of items' do
@@ -51,40 +50,41 @@ describe MyEnumerable do
 
   it 'returns collection sorted ascending' do
     expect(@e.sort_asc([])).to eq( [] )
-    expect(@e.sort_asc(@other_items)).to eq( [1, 3, 4, 4, 5, 6, 8, 82, 99] )
-    expect(@e.sort_asc(@items)).to eq( [0, 1, 1, 2, 2, 2, 3, 
-                                     3, 4, 4, 5, 5, 6, 6, 7] )
+    expect(@e.sort_asc(@other_items)).to eq( 
+      [1, 3, 4, 4, 5, 6, 8, 82, 99] )
+    expect(@e.sort_asc(@items)).to eq( 
+      [0, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7] )
   end
 
   it 'returns collection sorted descending' do    
     expect(@e.sort_desc([])).to eq( [] )
-    expect(@e.sort_desc(@other_items)).to eq( [99, 82, 8, 6, 5, 4, 4, 3, 1] )
-    expect(@e.sort_desc(@items)).to eq( [7, 6, 6, 5, 5, 4, 4,
-                                      3, 3, 2, 2, 2, 1, 1, 0] )
+    expect(@e.sort_desc(@other_items)).to eq( 
+      [99, 82, 8, 6, 5, 4, 4, 3, 1] )
+    expect(@e.sort_desc(@items)).to eq( 
+      [7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 2, 1, 1, 0] )
   end
 
-  it 'returns unique items in collection' do
-    expect(@e.uniq([])).to eq( [] )
-    expect(@e.uniq([1,2,1,2])).to eq( [] )
-    expect(@e.uniq(@other_items)).to eq( [1, 3, 5, 6, 8, 82, 99] )
-    expect(@e.uniq(@items)).to eq( [0, 1, 2, 3, 4, 5, 6, 7] )
+  it 'returns unique items in collection sorted descending' do
+    repetitous_collection = [1,1,4,4,5,6,7,7,8]
+    expect(@e.uniques([])).to eq( [] )
+    expect(@e.uniques([1,2,1,2])).to eq( [] )
+    expect(@e.uniques(@other_items)).to eq( [1, 3, 5, 6, 8, 82, 99] )
+    expect(@e.uniques(@items)).to eq( [0, 7] )
   end
 
   it 'rotates a 2d array 90 degrees counter-clockwise' do
-    # example that raises exception if 2d array is not passed
-    # example that raises exception if sub-arrays of equal length are not passed
+    # write example that raises exception if 2d array is not passed
+    # write example that raises exception if sub-arrays of equal length are not passed
     two_dee_ary = [ [1, 2, 3], [4, 5, 6], [7, 8, 9,] ]
 
-    expect(@e.send(:rotate_2d_ary, two_dee_ary)).to eq( [ [3, 6, 9], 
-                                                          [2, 5, 8], 
-                                                          [1, 4, 7] ] )
-    expect(@e.send(:rotate_2d_ary, two_dee_ary)).to eq( [ [5, 2, 0], [4, 6, 6],
-                                                          [3, 5, 7], [2, 3, 1], 
-                                                          [1, 4, 2] ] )
+    expect(@e.send(:rotate_2d_ary, two_dee_ary)).to eq( 
+      [ [3, 6, 9], [2, 5, 8], [1, 4, 7] ] )
+    expect(@e.send(:rotate_2d_ary, @items)).to eq( 
+      [ [5, 2, 0], [4, 6, 6], [3, 5, 7], [2, 3, 1], [1, 4, 2] ] )
   end
 
   it 'sums columns of 2d array' do
-    # example that raises exception if 2d array is not passed
+    # write example that raises exception if 2d array is not passed
     expect(@e.sum_columns(@items)).to eq( [7, 6, 15, 16, 7] )
   end
 
